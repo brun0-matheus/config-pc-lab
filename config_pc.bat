0906f2665ab9fb8c0b0a4c004b20838470371346
Bruno Matheus <brunomatheus2607@gmail.com>
2022-09-20 12:54:58 -0300

    Mudando ordem de instalacao

--- config_pc.bat
+++ config_pc.bat
@@ -1,9 +1,3 @@
-wsl --install -d Ubuntu
-
-set /P fds=Aperte enter quando o wsl terminar a instalacao
-
-wsl -e bash -c "sudo apt-get update && sudo apt-get install build-essential -y"
-
 set VSCODE_DOWNLOAD=%userprofile%\Downloads\vscode.zip
 curl https://az764295.vo.msecnd.net/stable/74b1f979648cc44d385a2286793c226e611f59e7/VSCode-win32-x64-1.71.2.zip --output %VSCODE_DOWNLOAD%
 
@@ -14,6 +8,8 @@ set SETTINGS_PATH=%appdata%\Code\User
 mkdir %SETTINGS_PATH%
 curl https://raw.githubusercontent.com/brun0-matheus/config-pc-lab/main/settings.json --output %SETTINGS_PATH%\settings.json
 
+wsl --install -d Ubuntu
 
+set /P fds=Aperte enter quando o wsl terminar a instalacao
 
-
+wsl -e bash -c "sudo apt-get update && sudo apt-get install build-essential gdb -y"
